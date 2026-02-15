06007df1132a307bdf5a773c35cdfddfc358b746
jaideep shetti <Jaideepshetti55@gmail.com>
2026-02-15 12:03:37 +0530

    second commit

--- firstcode.py
+++ firstcode.py
@@ -1,1 +1,11 @@
-print ('hello world ')
+print ("Jaideep\'s shetti")
+num1 = 15
+num2 = 12
+print('hello\'s  "jaideep"')
+print('hello ' *10)
+print('c :\\ navin')
+print('\njaideep\n' 'shetti\n' )
+print(num1 + num2)
+name = 'jaideep'
+print( "'num1 + num2\n'  name  +'\n shetti'")
+print(name  +'shetti')
---
+++ practice2.py
@@ -0,0 +1,2 @@
+text= 'jaideep'
+print(text *10)
